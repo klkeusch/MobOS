1166fe7e5b686bc519f8d9b692da71379f7f95ed
Klaus Keusch <hallo@klauskeusch.de>
2013-12-06 18:52:11 +0100

    Löschen der Testdatei - Erstellen der Rails-App

---
+++ mobos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobos::Application.config.secret_token = '7e98b3989d44e0878ef24d95978be002d663c49e147f2096a3a4f1ab1e3cc87542d7ed383579b3720ac8b1a880e4237894d3cfb1654f7ce7a6f353445b10af79'
